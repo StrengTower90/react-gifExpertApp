7cfdc9564d55db3d5630b2d83a738faae1a93f6f
Luis Escalante <luis.escalante.c0590@outlook.com>
2025-06-23 20:41:16 -0600

    changing api http to https

--- src/components/helpers/getGifs.js
+++ src/components/helpers/getGifs.js
@@ -2,7 +2,7 @@
 
 export const getGifs = async( Category ) => {
 
-    const url = `http://api.giphy.com/v1/gifs/search?q=${encodeURI( Category )}&limit=10&api_key=s3VV7F3ipSTEEFW1OQadZ7USdeSPk8O7`;
+    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI( Category )}&limit=10&api_key=s3VV7F3ipSTEEFW1OQadZ7USdeSPk8O7`;
     const resp = await fetch(url);
     const { data } = await resp.json();
 
